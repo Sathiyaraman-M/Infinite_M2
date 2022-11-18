1dc959ea22f5f7c296f3503277c92e05499b78e1
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-11-18 10:35:48 +0530

    Updated Explore page

---
+++ Infinite.Client/Pages/Explore.razor.cs
@@ -0,0 +1,26 @@
+﻿using Infinite.Base.Responses;
+
+namespace Infinite.Client.Pages;
+
+public partial class Explore
+{
+    private List<MinifiedProjectResponse> _recentlyAddedProjects = new();
+    private List<MinifiedBlogResponse> _recentlyAddedBlogs = new();
+    private UserProfileInfoResponse _userprofile;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await ManageAccountHttpClient.GetUserProfileInfo();
+        if (result.Succeeded)
+        {
+            _userprofile = result.Data;
+        }
+        else
+        {
+            foreach (var error in result.Messages)
+            {
+                Toast.Add("Error", error, Severity.Error);
+            }
+        }
+    }
+}
