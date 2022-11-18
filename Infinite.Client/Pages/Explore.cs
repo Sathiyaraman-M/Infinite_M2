25a295f61ed2a6e57959d481d6e75ab7d56f39d2
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-11-18 10:36:07 +0530

    Added CustomCarousel component

--- Infinite.Client/Pages/Explore.cs
+++
@@ -1,9 +0,0 @@
-﻿using Infinite.Base.Responses;
-
-namespace Infinite.Client.Pages;
-
-public partial class Explore
-{
-    private List<MinifiedProjectResponse> _recentlyAddedProjects = new();
-    private List<MinifiedBlogResponse> _recentlyAddedBlogs = new();
-}
